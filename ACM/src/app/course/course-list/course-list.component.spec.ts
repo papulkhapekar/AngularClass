a154f2da9e734fbfaa40a503963492237cf0cb42
papul khapekar <papukhapekar@gmail.com>
2019-09-29 08:49:30 +0530

    course list componet test case writtern

--- ACM/src/app/course/course-list/course-list.component.spec.ts
+++ ACM/src/app/course/course-list/course-list.component.spec.ts
@@ -1,8 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CourseListComponent } from './course-list.component';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
 import { StarComponent } from 'src/app/shared/star/star.component';
-import { HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 describe('CourseListComponent', () => {
   let component: CourseListComponent;
@@ -10,9 +17,13 @@ describe('CourseListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientModule],
-      declarations: [ CourseListComponent,
-      StarComponent ]
+      imports: [RouterTestingModule,
+        HttpClientTestingModule,
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig)
+],
+      declarations: [ CourseListComponent, StarComponent]
     })
     .compileComponents();
   }));
--- ACM/src/test.ts
+++ ACM/src/test.ts
@@ -16,6 +16,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /app.component.spec\.ts$/);
+const context = require.context('./', true, /course-list.component.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
